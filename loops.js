e19343dc5ce5ef93a44d85c010a91e39b2c9b416
Stanley Huang Huang <stanhua33@gmail.com>
2017-07-23 01:32:21 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop(array) {
+for (let i = 0; i < 25; i++) {
+  if (i === 1) {
+    array.unshift("I am 1 strange loop.")
+  }
+  else {
+    array.unshift("I am " + i + " strange loops.")
+  }
+}
+return array
+}
+
+function whileLoop(n) {
+    while (n > 0) {
+      if (n === 0) {
+        console.log('done')
+      }
+      else {
+        console.log(n)
+      }
+      --n
+    }
+  return n
+}
